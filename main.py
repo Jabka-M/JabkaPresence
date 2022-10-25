99cb18abbcbfc11ecb08cf656ab82d4b9e45288d
Someone <66884122+Jabka-M@users.noreply.github.com>
2022-10-26 00:39:00 +0300

    v2 release

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pypresence
-import pyautogui
 import ctypes
+import ctypes.wintypes
 import configparser
+import re
 
 class Blacklist(object):
     def __init__(self, blacklist: list = [], strict: bool = False):
@@ -13,7 +14,7 @@ class Blacklist(object):
 
 class App(pypresence.Presence):
     def __init__(self, appId):
-        super(App, self).__init__(client_id=appId)
+        super(App, self).__init__(client_id=int(appId))
         self.lastActivity = None
         self.config = configparser.ConfigParser()
         self.blacklist = Blacklist()
@@ -39,6 +40,9 @@ class App(pypresence.Presence):
 
         return None
 
+    def findPattern(self, pattern, string):
+        return (result := re.compile(pattern.replace("?", ".")).search(string)) and result.groups() or False
+
     def buildButtons(self):
             buttons = []
             if self.button1Text and self.button1URL:
@@ -49,6 +53,9 @@ class App(pypresence.Presence):
             return buttons
 
     def updateWikipedia(self, wikipedia=None):
+        if not self.showWikipedia:
+            return False
+        
         if not wikipedia:
             wikipedia = self._getWindowByName("— Википедия - Google Chrome") or self._getWindowByName("- Wikipedia - Google Chrome")
 
@@ -66,11 +73,41 @@ class App(pypresence.Presence):
         self.lastActivity = wikipedia
         return True
 
+    def updateJutsu(self, jutsu=None):
+        if not self.showJutsu:
+            return False
+
+        if not jutsu:
+            jutsu = self._getWindowByName("Jut.su - Google Chrome")
+
+        if not jutsu:
+            return False
+
+        elif jutsu == self.lastActivity:
+            return True
+        
+        if self.blacklist.check(jutsu):
+            return False
+        
+        result = self.findPattern("Смотреть (?*) ([\d]+ серия|[\d]+ фильм) на", jutsu)
+        if not result:
+            return False
+
+        anime, episode = result
+        buttons = self.buildButtons()
+        self.update(details="Смотрит аниме", state=f"{anime} {episode}", large_image=self.jutsuLargeImage, large_text=self.jutsuLargeImageText, small_image=self.jutsuSmallImage, small_text=self.jutsuSmallImageText, buttons=buttons, instance=False)
+        self.lastActivity = jutsu
+        return True
+
     def updateYouTube(self, youtube=None):
+        if not self.showYoutube:
+            return False
+
         if not youtube:
             youtube = self._getWindowByName("- YouTube - Google Chrome")
 
         if not youtube:
+            self.lastActivity = None
             return False
 
         elif youtube == self.lastActivity:
@@ -79,7 +116,7 @@ class App(pypresence.Presence):
         if self.blacklist.check(youtube):
             return False
 
-        if "lyric" in youtube.lower() or "nightcore" in youtube.lower() or "song" in youtube.lower():
+        if "lyric" in youtube.lower() or "nightcore" in youtube.lower() or "song" in youtube.lower() or "audio" in youtube.lower():
             text = f"Слушает {youtube}"
 
         else:
@@ -91,81 +128,80 @@ class App(pypresence.Presence):
         return True
 
     def updateCurrentWindow(self):
-        try:
-            currentWindow = pyautogui.getActiveWindowTitle()
-            
-        except:
+        if not self.showCurrent:
+            return False
+
+        hwnd = ctypes.windll.user32.GetForegroundWindow()
+        length = ctypes.windll.user32.GetWindowTextLengthW(hwnd) + 1
+        windowTitle = ctypes.create_unicode_buffer(length)
+        ctypes.windll.user32.GetWindowTextW(hwnd, windowTitle, length)
+        currentWindow = windowTitle.value
+
+        if not currentWindow or currentWindow == self.lastActivity:
             return
 
-        else:
-            if not currentWindow or currentWindow == self.lastActivity:
-                return
+        elif self.blacklist.check(currentWindow):
+            return
 
-            elif self.blacklist.check(currentWindow):
-                return
-            
-            elif "- YouTube - Google Chrome" in currentWindow:
-                return self.updateYouTube(currentWindow.replace("- YouTube - Google Chrome", "").strip())
+        elif "Jut.su - Google Chrome" in currentWindow:
+            return self.updateJutsu(currentWindow.replace("Jut.su - Google Chrome", "").strip())
 
-            buttons = self.buildButtons()
-            self.update(details="Сейчас в", state=currentWindow, large_image=self.currentLargeImage, large_text=self.currentLargeImageText, small_image=self.currentSmallImage, small_text=self.currentSmallImageText, buttons=buttons, instance=False)
-            self.lastActivity = currentWindow
+        elif "- YouTube - Google Chrome" in currentWindow:
+            return self.updateYouTube(currentWindow.replace("- YouTube - Google Chrome", "").strip())
+
+        elif "— Википедия - Google Chrome" in currentWindow or "- Wikipedia - Google Chrome" in currentWindow:
+            return self.updateWikipedia(currentWindow.replace("- Wikipedia - Google Chrome", "").replace("— Википедия - Google Chrome", "").strip())
+
+        buttons = self.buildButtons()
+        self.update(details="Сейчас в", state=currentWindow, large_image=self.currentLargeImage, large_text=self.currentLargeImageText, small_image=self.currentSmallImage, small_text=self.currentSmallImageText, buttons=buttons, instance=False)
+        self.lastActivity = currentWindow
 
     def loadConfig(self, filename):
         self.config.read(filename)
         self.blacklist = Blacklist(blacklist=([i.strip() for i in self.config.get("PRIVACY", "blacklist").split(";") if i != ""] or []), strict=(self.config.getboolean("PRIVACY", "strict") or False))
-        self.showYoutube = self.config.getboolean("MAIN", "show-youtube") or False
-        self.showCurrent = self.config.getboolean("MAIN", "show-current") or False
-        self.showWikipedia = self.config.getboolean("MAIN", "show-wikipedia") or False
-        self.youtubeLargeImage = self.config.get("YOUTUBE", "youtube-large-image") or None
-        self.youtubeSmallImage = self.config.get("YOUTUBE", "youtube-small-image") or None
-        self.youtubeLargeImageText = self.config.get("YOUTUBE", "youtube-large-image-text") or None
-        self.youtubeSmallImageText = self.config.get("YOUTUBE", "youtube-small-image-text") or None
-        self.currentLargeImage = self.config.get("CURRENT", "current-large-image") or None
-        self.currentSmallImage = self.config.get("CURRENT", "current-small-image") or None
-        self.currentLargeImageText = self.config.get("CURRENT", "current-large-image-text") or None
-        self.currentSmallImageText = self.config.get("CURRENT", "current-small-image-text") or None
-        self.wikipediaLargeImage = self.config.get("WIKIPEDIA", "wikipedia-large-image") or None
-        self.wikipediaSmallImage = self.config.get("WIKIPEDIA", "wikipedia-small-image") or None
-        self.wikipediaLargeImageText = self.config.get("WIKIPEDIA", "wikipedia-large-image-text") or None
-        self.wikipediaSmallImageText = self.config.get("WIKIPEDIA", "wikipedia-small-image-text") or None
+        self.showJutsu = self.config.getboolean("MAIN", "jutsu") or False
+        self.showYoutube = self.config.getboolean("MAIN", "youtube") or False
+        self.showCurrent = self.config.getboolean("MAIN", "current") or False
+        self.showWikipedia = self.config.getboolean("MAIN", "wikipedia") or False
+        self.jutsuLargeImage = self.config.get("JUTSU", "large-image") or None
+        self.jutsuSmallImage = self.config.get("JUTSU", "small-image") or None
+        self.jutsuLargeImageText = self.config.get("JUTSU", "large-image-text") or None
+        self.jutsuSmallImageText = self.config.get("JUTSU", "small-image-text") or None
+        self.youtubeLargeImage = self.config.get("YOUTUBE", "large-image") or None
+        self.youtubeSmallImage = self.config.get("YOUTUBE", "small-image") or None
+        self.youtubeLargeImageText = self.config.get("YOUTUBE", "large-image-text") or None
+        self.youtubeSmallImageText = self.config.get("YOUTUBE", "small-image-text") or None
+        self.currentLargeImage = self.config.get("CURRENT", "large-image") or None
+        self.currentSmallImage = self.config.get("CURRENT", "small-image") or None
+        self.currentLargeImageText = self.config.get("CURRENT", "large-image-text") or None
+        self.currentSmallImageText = self.config.get("CURRENT", "small-image-text") or None
+        self.wikipediaLargeImage = self.config.get("WIKIPEDIA", "large-image") or None
+        self.wikipediaSmallImage = self.config.get("WIKIPEDIA", "small-image") or None
+        self.wikipediaLargeImageText = self.config.get("WIKIPEDIA", "large-image-text") or None
+        self.wikipediaSmallImageText = self.config.get("WIKIPEDIA", "small-image-text") or None
         self.button1Text = self.config.get("BUTTONS", "button-1-text") or None
         self.button1URL = self.config.get("BUTTONS", "button-1-url") or None
         self.button2Text = self.config.get("BUTTONS", "button-2-text") or None
         self.button2URL = self.config.get("BUTTONS", "button-2-url") or None
 
     def mainloop(self):
+        print(f"Started with settings:\nJutsu: {self.showJutsu}\nYouTube: {self.showYoutube}\nCurrent: {self.showCurrent}\nWikipedia: {self.showWikipedia}")
         while True:
-            if self.showYoutube:
+            if not self.updateJutsu():
                 if not self.updateYouTube():
-                    if self.showWikipedia:
-                        if not self.updateWikipedia():
-                            if self.showCurrent:
-                                self.updateCurrentWindow()
-                                
-                            else:
-                                self.clear()
-                                
-                    else:
-                        self.clear()
-                                
-            elif self.showWikipedia:
-                if not self.updateWikipedia():
-                    if self.showCurrent:
-                        self.updateCurrentWindow()
-                       
-                    else:
-                        self.clear()
-                        
-            elif self.showCurrent:
-                self.updateCurrentWindow()
+                    if not self.updateWikipedia():
+                        if not self.updateCurrentWindow():
+                            self.clear()
 
 
 try:
-    app = App(your_app_id) # get it on discord.com/developers/applications
+    print("Creating app...")
+    app = App(your_app_id)
+    print("Connecting...")
     app.connect()
+    print("Loading config...")
     app.loadConfig("./config.ini")
     app.mainloop()
 except Exception as e:
-    pyautogui.alert(text=repr(e), title="Jabka Activity Error")
+    print(repr(e))
     exit(1)
